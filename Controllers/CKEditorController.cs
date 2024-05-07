1d47854d9702f93458d97ec857ba0044ff45760e
Portfolio1 <rutvisavaliya16@gmail.com>
2024-05-07 15:58:20 +0530

    Initial commit

---
+++ Controllers/CKEditorController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication8.Controllers
+{
+    public class CKEditorController : Controller
+    {
+        // GET: CKEditor
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult UploadImage(HttpPostedFileBase upload)
+        //{
+        //    if (upload != null && upload.ContentLength > 0)
+        //    {
+        //        var fileName = Path.GetFileName(upload.FileName);
+        //        var path = Path.Combine(Server.MapPath("~/Content/uploads/Images/"), fileName);
+        //        upload.SaveAs(path);
+        //        return Json(new { uploaded = true, url = Url.Content("~/Content/uploads/Images/" + fileName) });
+        //    }
+        //    return Json(new { uploaded = false, error = new { message = "No file uploaded." } });
+        //}
+        //[HttpPost]
+        //public ActionResult UploadImage1(HttpPostedFileBase file)
+        //{
+        //    if (file != null && file.ContentLength > 0)
+        //    {
+        //        // Save the uploaded image to the server
+        //        string fileName = Path.GetFileName(file.FileName);
+        //        string filePath = Path.Combine(Server.MapPath("~/Content/uploads/Images/"), fileName);
+        //        file.SaveAs(filePath);
+
+        //        // Return the URL of the uploaded image
+        //        string imageUrl = Url.Content("~/Content/uploads/Images/" + fileName);
+        //        return Content(imageUrl);
+        //    }
+
+        //    // Handle error case
+        //    return Content("");
+        //}
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+
+namespace WebApplication8.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+
+
+        public ActionResult UploadImage(HttpPostedFileBase upload)
+        {
+            if (upload != null && upload.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(upload.FileName);
+                var path = Path.Combine(Server.MapPath("~/Content/uploads/Images/"), fileName);
+                upload.SaveAs(path);
+                return Json(new { uploaded = true, url = Url.Content("~/Content/uploads/Images/" + fileName) });
+            }
+            return Json(new { uploaded = false, error = new { message = "No file uploaded." } });
+        }
+
+
+    }
+}
